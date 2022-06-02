dc8f4ef68d790b4fd04ace61a8e4610fe38654e1
strbytes <strbytes@gmail.com>
2022-06-02 10:02:52 -0400

    Refactor translate_algebraic and Player.make_move
    A dictionary is now returned from translate_algebraic with names for
    each potential value it can return, which are matched to
    Player.make_move parameters with star notation. Also added case handling
    to Player.make_move for special coordinate inputs (castling, promotion,
    file and rank specifiers).

--- chess.py
+++ chess.py
@@ -14,7 +14,7 @@ class Game:
         player = self.whose_turn
         if coord:
             # automatic turn play via function call for testing purposes
-            player.make_move(*translate_algebraic(coord))
+            player.make_move(**translate_algebraic(coord))
             self.turn += 1
             return
         print(self.board)
@@ -30,42 +30,58 @@ class Game:
 
 
 def translate_algebraic(alg_coord):
+    translated = {
+        entry: None
+        for entry in ["piece_type", "coord", "file", "rank", "promotion", "castle_side"]
+    }
+    if alg_coord[0:3] == "O-O" or alg_coord[0:3] == "0-0":
+        if alg_coord[3:] == "-O" or alg_coord[3:] == "-0":
+            translated["castle_side"] = "kingside"
+        else:
+            translated["castle_side"] = "queenside"
     pieces = {"K": King, "Q": Queen, "R": Rook, "B": Bishop, "N": Knight}
     xs = "abcdefgh"
     ys = "12345678"
-    file, rank = None, None
     if len(alg_coord) == 2:
-        piece = Pawn
+        translated["piece_type"] = Pawn
         coord = alg_coord
     elif len(alg_coord) == 3:
         if alg_coord[0] in pieces:
-            piece = pieces[alg_coord[0]]
+            translated["piece_type"] = pieces[alg_coord[0]]
+            coord = alg_coord[1:]
+        elif alg_coord[-1] in pieces:
+            translated["piece_type"] = Pawn
+            translated["promotion"] = pieces[alg_coord[-1]]
+            coord = alg_coord[:2]
         elif alg_coord[0] in xs:
-            piece = Pawn
-            file = xs.index(alg_coord[0])
+            translated["piece_type"] = Pawn
+            coord = alg_coord[1:]
+            translated["file"] = xs.index(alg_coord[0])
         elif alg_coord[0] in ys:
-            piece = Pawn
-            rank = ys.index(alg_coord[0])
+            translated["piece_type"] = Pawn
+            translated["rank"] = ys.index(alg_coord[0])
+            coord = alg_coord[1:]
         else:
             raise ValueError(
                 f"invalid character {alg_coord[0]} in coordinate {alg_coord}"
             )
-        coord = alg_coord[1:]
     elif len(alg_coord) == 4:
-        piece = pieces[alg_coord[0]]
+        translated["piece_type"] = pieces[alg_coord[0]]
         if alg_coord[1] in xs:
-            file = xs.index(alg_coord[1])
+            translated["file"] = xs.index(alg_coord[1])
         elif alg_coord[1] in ys:
-            rank = ys.index(alg_coord[1])
+            translated["rank"] = ys.index(alg_coord[1])
         else:
             raise ValueError(
                 f"invalid character {alg_coord[1]} in coordinate {alg_coord}"
             )
         coord = alg_coord[2:]
     elif len(alg_coord) == 5:
-        piece = pieces[alg_coord[0]]
+        translated["piece_type"] = pieces[alg_coord[0]]
         if alg_coord[1] in xs and alg_coord[2] in ys:
-            file, rank = xs.index(alg_coord[1]), ys.index(alg_coord[2])
+            translated["file"], translated["rank"] = xs.index(alg_coord[1]), ys.index(
+                alg_coord[2]
+            )
         else:
             raise ValueError(
                 f"invalid characters {alg_coord[1:3]} in coordinate {alg_coord}"
@@ -73,7 +89,9 @@ def translate_algebraic(alg_coord):
         coord = alg_coord[3:]
     else:
         raise ValueError(f"invalid coordinate {alg_coord}")
-    return piece, (xs.index(coord[0]), ys.index(coord[1])), file, rank
+    if coord:
+        translated["coord"] = xs.index(coord[0]), ys.index(coord[1])
+    return translated
 
 
 class Board:
@@ -220,14 +238,28 @@ class Player:
             if isinstance(piece, King):
                 self.king = piece
 
-    def make_move(self, type, coord, file=None, rank=None):
+    def make_move(self, piece_type, coord, file, rank, promotion, castle_side):
+        if castle_side:
+            self.castle(castle_side)
+            return
         can_move = []
         for p in self.pieces:
-            if isinstance(p, type) and p.pos and coord in p.legal_moves:
-                can_move.append(p)
+            if isinstance(p, piece_type) and p.pos and coord in p.legal_moves:
+                if file and rank:
+                    if p.pos == (file, rank):
+                        can_move.append(p)
+                elif file:
+                    if p.pos[0] == file:
+                        can_move.append(p)
+                elif rank:
+                    if p.pos[1] == rank:
+                        can_move.append(p)
+                else:
+                    can_move.append(p)
         if len(can_move) == 1:
             piece = can_move[0]
             if piece.type == "pawn":
+                # if double stepping, remember which turn it happened
                 if piece.moved == False and abs(piece.pos[1] - coord[1]) == 2:
                     piece.double_step = self.board.game.turn
                 # if x axis changes, means diagonal step
@@ -236,12 +268,29 @@ class Player:
                     if self.board[coord] is None:
                         # remove pawn being taken en passant
                         self.board.remove_piece((coord[0], piece.pos[1]))
+                # if pawn moving to last row, check if promotion is specified and promote if so
+                if coord[1] == (7 if piece.player.color == "white" else 0):
+                    if not promotion:
+                        raise ValueError("please specify promotion")
+                    self.promote(piece, coord, promotion)
+                    return
+                # promotion shouldn't be specified unless moving to last row
+                elif promotion:
+                    raise ValueError("Promotion specified when inappropriate")
             self.board.move_piece(piece, coord)
             piece.moved = True
         elif len(can_move) > 1:
             raise ValueError(f"multiple pieces can make that move: {can_move}")
         else:
-            raise ValueError(f"No pieces of type {type} can move to {coord}")
+            raise ValueError(f"No pieces of type {piece_type} can move to {coord}")
+
+    def castle(self, castle_side):
+        # TODO
+        raise NotImplementedError
+
+    def promote(self, piece, coord, promotion):
+        # TODO
+        raise NotImplementedError
 
     def __getitem__(self, item):
         return self.pieces_dict[item]
